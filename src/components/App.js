1ade8b155b9aa2a5ca44dc22624e49602d57d738
bdestasio33 <bdestasio33@gmail.com>
2020-01-15 22:05:31 -0500

    working parent

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import "../assets/css/App.css";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "../components/HomePage";
+import LoginComponent from "./LoginComponent";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route path="/" component={HomePage} />
+          <Route path="/" component={LoginComponent} />
           <Route path="/Home" component={HomePage} />
         </Switch>
       </Router>
--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,7 +1,61 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Input, AppBar, Typography, TextField } from "@material-ui/core";
+import * as passwordHash from "password-hash";
+import * as adminUser from "../authorization/admin.json";
 
 const LoginComponent = () => {
-  return;
+  //console.log(adminUser.default.adminLogin);
+  return (
+    <div>
+      <AppBar position="static" style={{ marginBottom: "10vw" }}>
+        <Typography variant="h6">Login</Typography>
+      </AppBar>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = "Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setTimeout(() => {
+            console.log(values);
+            if (
+              values.email === adminUser.default.adminLogin.Email &&
+              passwordHash.verify(
+                values.password,
+                adminUser.default.adminLogin.Password
+              ) === true
+            ) {
+              alert("Succesful Admin Login");
+            } else {
+              alert("failed Login");
+            }
+            resetForm();
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
 
 export default LoginComponent;
